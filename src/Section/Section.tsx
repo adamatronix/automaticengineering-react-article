b89edd9e228682c65b153a9b34ac0daec2d6512a
adamatronix <you@example.com>
2023-01-30 16:11:47 +0800

    RA-679 Fix baseSrc issue with vite and ts. Add styled component types.

--- src/Section/Section.tsx
+++ src/Section/Section.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { media } from '../utils/mediaQuery';
+import { media } from 'utils/mediaQuery';
 
 interface SectionProps {
   children: React.ReactNode;
