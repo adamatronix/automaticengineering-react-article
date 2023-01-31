ceb6456484abdb41483b92616c46e59a73520ca9
adamatronix <you@example.com>
2023-01-31 14:25:00 +0800

    RA-cc1 Add clickable section with prev and next icons

--- src/Carousel/Carousel.stories.tsx
+++ src/Carousel/Carousel.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Carousel } from './Carousel';
 import imageOne from 'ImageBlock/assets/20201219_DevHynes_00055.jpeg';
@@ -13,8 +14,12 @@ export default {
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } as ComponentMeta<typeof Carousel>;
 
+
+const Spacer = styled.div`
+  height: 800px;
+`
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Carousel> = (args) => <Carousel {...args} />;
+const Template: ComponentStory<typeof Carousel> = (args) => <><Carousel {...args} /></>;
 
 export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
