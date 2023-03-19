7f09a319682b08938ecbb5f6ada71b335ff2177d
adamatronix <you@example.com>
2023-03-19 18:02:32 +0800

    H-5e1 Pass pullQuoteCss block to pullQuote

--- src/PullQuote/PullQuote.tsx
+++ src/PullQuote/PullQuote.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { CSSProp } from 'styled-components';
 import { Section } from 'Section/Section';
 import { fontstack } from 'utils/fontstack';
 import { type } from 'utils/type';
 
 
-const PullQuoteText = styled.div`
+const PullQuoteText = styled.div<{cssBlock?: CSSProp}>`
   ${fontstack.default}
   ${type('heading03')}
   color: rgb(var(--article-primary));
@@ -21,19 +21,23 @@ const PullQuoteText = styled.div`
     display: inline-block;
   }
 
+  ${({cssBlock}) => cssBlock || null}
+
 `
 
 interface PullQuoteProps {
+  pullQuoteCss?: CSSProp,
   children: React.ReactNode,
 }
 
 export const PullQuote = ({
+  pullQuoteCss,
   children,
   ...props
 }: PullQuoteProps) => {
   return (
     <Section contain {...props}>
-      <PullQuoteText>{children}</PullQuoteText>
+      <PullQuoteText cssBlock={pullQuoteCss}>{children}</PullQuoteText>
     </Section>
   )
 }
