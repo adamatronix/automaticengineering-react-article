1d3313cb76362d49df3a9699f2de5fe744e4fed4
adamatronix <you@example.com>
2023-03-19 17:50:44 +0800

    H-5e1 Pass caotionCss to Caption

--- src/Caption/Caption.tsx
+++ src/Caption/Caption.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { CSSProp } from 'styled-components';
 import { fontstack } from 'utils/fontstack';
 import { type } from 'utils/type';
 
 
-const CaptionText = styled.div`
+const CaptionText = styled.div<{cssBlock?: CSSProp}>`
   ${fontstack.default}
   ${type('caption01')}
   margin: 10px 0;
+  ${({cssBlock}) => cssBlock || null}
 `
 
 interface CaptionProps {
+  captionCss?: CSSProp,
   children: React.ReactNode,
 }
 
 export const Caption = ({
+  captionCss,
   children,
   ...props
 }: CaptionProps) => {
   return (
-    <CaptionText {...props}>{children}</CaptionText>
+    <CaptionText cssBlock={captionCss} {...props}>{children}</CaptionText>
   )
 }
